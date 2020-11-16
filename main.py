3d9c9d22343e4e7c920b7c007f6a895dbd1eaad2
Brian Phillips <brian@briancphillips.com>
2020-11-16 10:31:32 -0600

    changes

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from filemanager import FileManager
 
 
 if __name__ == '__main__':
-    #if len(sys.argv) != 2:
-        #print('No torrent file specified.\nUsage: "python main.py <torrent filename>"')
-        #sys.exit(1)
-    filename = "big-buck-bunny.torrent"
+    if len(sys.argv) != 2:
+        print('No torrent file specified.\nUsage: "python main.py <torrent filename>"')
+        sys.exit(1)
+    filename = sys.argv[1]
     torrent = ParseTorrent(filename).parse()
     tc = TrackerConnect(torrent)
     conn = Connection(tc, torrent)
